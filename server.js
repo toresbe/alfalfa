7c32fcb64dd59a17026a7d46298c733d88e44ea9
Tore Sinding Bekkedal <toresbe@protonmail.com>
2021-02-06 22:33:38 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+    console.log('Connected');
+    ws.on('message', function incoming(data) {
+        console.log(data);
+        wss.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(data);
+            }
+        });
+    });
+}); 
