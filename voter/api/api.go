2080d0c91f5fe6fe10a09c25bbd58ec9cdc7f6a9
Tore Sinding Bekkedal <toresbe@protonmail.com>
2021-02-14 23:23:48 +0100

    further refactoring

--- voter/api/api.go
+++ voter/api/api.go
@@ -1,1 +1,41 @@
 package api
+
+import (
+	"../meeting"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
+	"net/http"
+)
+type App struct {
+	Router *mux.Router
+}
+
+func (a *App) meetingCreate(w http.ResponseWriter, r *http.Request) {
+	var p meeting.Meeting
+	err := json.NewDecoder(r.Body).Decode(&p)
+	if err != nil {
+		http.Error(w, "JSON decode error: " + err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	log.WithFields(log.Fields{
+		"meetingName": p.Name,
+		"attendants": p.Attendants,
+	}).Info(`meeting created`)
+}
+
+
+
+func (a *App) initializeRoutes() {
+	a.Router.HandleFunc("/api/v1/meetings", a.meetingCreate).Methods("POST")
+}
+
+func (a *App) Initialize() {
+	a.Router = mux.NewRouter()
+	a.initializeRoutes()
+}
+
+func (a *App) Run(addr string) {
+	log.Fatal(http.ListenAndServe(addr, a.Router))
+}
--- voter/main.go
+++ voter/main.go
@@ -1,21 +1,13 @@
 package main
 
 import (
-    "encoding/json"
-    "github.com/gorilla/mux"
     log "github.com/sirupsen/logrus"
-    "net/http"
+
     "time"
-    "./meeting"
+    "./api"
 )
 
-type App struct {
-    Router *mux.Router
-}
-
 type Attendant int
-
-
 
 type Vote struct {
     choice      int
@@ -40,37 +32,12 @@ func (d *Decision) AddVote(v *Vote) {
 func (d *Decision) GetStatus() {
 }
 
-func (a *App) meetingCreate(w http.ResponseWriter, r *http.Request) {
-    var p meeting.Meeting
-    err := json.NewDecoder(r.Body).Decode(&p)
-    if err != nil {
-        http.Error(w, "JSON decode error: " + err.Error(), http.StatusBadRequest)
-        return
-    }
 
-    log.WithFields(log.Fields{
-        "meetingName": p.Name,
-        "attendants": p.Attendants,
-    }).Info(`meeting created`)
-}
 
 func main() {
     log.SetFormatter(&log.TextFormatter{})
 
-    a := App{}
+    a := api.App{}
     a.Initialize()
     a.Run(":10000")
-}
-
-func (a *App) initializeRoutes() {
-    a.Router.HandleFunc("/api/v1/meetings", a.meetingCreate).Methods("POST")
-}
-
-func (a *App) Initialize() {
-    a.Router = mux.NewRouter()
-    a.initializeRoutes()
-}
-
-func (a *App) Run(addr string) {
-    log.Fatal(http.ListenAndServe(addr, a.Router))
 }
